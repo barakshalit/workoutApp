d80d15a80f5ffdf514bb98eb66dbf80ab08bd1f9
barakshalit <shalitbarak@gmail.com>
2025-06-14 20:05:51 +0300

    index update

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function loadExerciseOptions() {
   fetch("https://script.google.com/macros/s/AKfycbyToRzGrh-PhFltrVW45GOjmVO3MBgeks4dlMFLZ7kwgG3ImqkAxGapaoxPBA1EF89FeA/exec")
     .then(res => res.json())
     .then(exercises => {
-      alert("after fetch");
       const select = document.getElementById("exercise");
       select.innerHTML = ""; // Clear default option
 
